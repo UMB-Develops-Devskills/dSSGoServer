3491956a20d9a24270ca75ac8e0346290f0f7aa7
Stephanie <stephanieha2021@gmail.com>
2026-06-23 15:55:52 -0400

    routes json file

--- cmd/devskills/main.go
+++ cmd/devskills/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"log"
+	"time"
 	"net/http"
 	"os"
+	"flag"
 	"context"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	//"github.com/go-chi/docgen"
 	"github.com/go-chi/render"
 	//"github.com/joho/godotenv"
+	"github.com/go-chi/docgen"
 	"github.com/bettaburger/dSSGoServer/internal/handlers"
 	"github.com/bettaburger/dSSGoServer/internal/cloudsql"
 
 )
+
+var routes = flag.Bool("routes", false, "Generate router documentation")
 
 func paginate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -29,24 +34,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}*/
-	ctx := context.Background()
-	
-	// Connect to Cloud sql 
-	db, cleanup, err := cloudsql.ConnectDB(ctx)
-	if err != nil {
-		log.Printf("DB connection failed: %v", err)
-	}
-	defer cleanup()
-	defer db.Close()
-
-	handler := &handlers.TaskHandler{DB : db}
-
+	flag.Parse()
 	// standard middleware
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Use(middleware.Timeout(120 * time.Second)) // timeout value of 2 minute 
 
 	// endpoints
 	r.Get("/", func(w http.ResponseWriter, r*http.Request) {w.Write([]byte("This is the devskills root server"))})
@@ -57,12 +52,32 @@ func main() {
 	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {w.Write([]byte("pong"))})
 	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {panic("test")})
 
+	if *routes {
+		fmt.Println(docgen.JSONRoutesDoc(r))
+		fmt.Println(docgen.MarkdownRoutesDoc(r, docgen.MarkdownOpts{
+			ProjectPath: "github.com/bettaburger/dSSGoServer",
+		}))
+		return
+	}
+
+	ctx := context.Background()
+	// Connect to Cloud sql 
+	db, cleanup, err := cloudsql.ConnectDB(ctx)
+	if err != nil {
+		log.Printf("DB connection failed: %v", err)
+	}
+	defer cleanup()
+	defer db.Close()
+
+	handler := &handlers.TaskHandler{DB : db}
+
 	// REST routes /api
 	r.Route("/api", func(r chi.Router) {
 		//r.With(paginate).Get("/", handlers.GetJobData)
 		r.Get("/jobs", handler.GetJobData) // GET /api/jobs
 		// add more methods later
 	})
+
 	// cloudsql runs on default port 8080
 	port := os.Getenv("PORT")
 	if port == "" {
